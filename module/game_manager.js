f8f19c662c74add931627b601b0a542b67faffa4
doberankei <masayathegantz@gmail.com>
2018-10-14 00:38:52 +0900

    勝敗判定作成

--- module/game_manager.js
+++ module/game_manager.js
@@ -1,2 +1,5 @@
 var board = new Board();
+var game_judge = new GameJudge();
 board.draw();
+
+console.log(game_judge.judge(board.get_board()));
--- module/judge.js
+++ module/judge.js
@@ -1,10 +1,52 @@
 
-class Judge {
+/**
+  * 判定するもの
+  * - 勝ち負け GameJudge
+  * - 置けるます
+  * - 
+  **/
+class GameJudge {
+  constructor(){}
+  
+  judge(cells){
+    let black_count = 0;
+    let white_count = 0;
+    let none_count = 0;
+    for(var cell_y = 0; cell_y < 8; cell_y++){
+      for(var cell_x = 0; cell_x < 8; cell_x++){
+        switch(cells[cell_y][cell_x]){
+          case STONE.NONE:
+            none_count++;
+            break;
+          case STONE.BLACK:
+            black_count++;
+            break;
+          case STONE.WHITE:
+            white_count++;
+            break;
+        }
+      }
+    }
+    if(black_count == 0){
+      return STONE.WHITE;
+    }
+    if(white_count == 0){
+      return STONE.BLACK;
+    }
+    if(none_count == 0){
+      if(white_count < black_count){
+        return STONE.BLACK;
+      }
+      if(black_count < white_count){
+        return STONE.WHITE;
+      }
+    }
+    return STONE.NONE;
+  }
+}
+
+
+class PutCellJudge {
   constructor(){
   }
-  
-  
-  
-  
-  
 }
